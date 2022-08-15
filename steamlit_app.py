e45ab6f058d65ffabc3f9dab67f646facf859c74
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 23:04:52 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -19,16 +19,8 @@ dataset_url ="https://github.com/amalbinessa/re_streamlit/blob/main/Data/T_dataf
 
 import csv
 
-with open(dataset_url) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+with open(dataset_url, mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-st.write(df) 
+
+
