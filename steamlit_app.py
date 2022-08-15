44b8454aeb322b3dd804cb287857b336f796a68f
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 22:19:01 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -11,20 +11,16 @@ st.set_page_config(
 st.title("ابحث عنك في قوقل")
 
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     st.write(bytes_data)
+dataset_url = "./Data/T_dataframe.xlsx"
 
-     # To convert to a string based IO:
-     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-     st.write(stringio)
+# read csv from a URL
 
-     # To read file as string:
-     string_data = stringio.read()
-     st.write(string_data)
+def get_data() -> pd.DataFrame:
+    return pd.read_excel(dataset_url)
 
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
-     st.write(dataframe)
+df = get_data()
+
+st.write(df) 
+
+
+
