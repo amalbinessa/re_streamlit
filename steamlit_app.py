a4abcad12c35977160de19f1b14b7fe7b228d93e
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 22:10:19 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -9,3 +9,13 @@ st.set_page_config(
 )
 
 st.title("ابحث عنك في قوقل")
+
+
+def file_selector(folder_path='./Data'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+filename = file_selector()
+st.write('You selected `%s`' % filename)
+
