2df3c5a05f22b3ba526418d88a81b81e4f3608ac
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 23:22:54 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -39,17 +39,6 @@ for link in links:
           
           print("------")
           title_link_list.append(title_list)
-dataset_url ="https://github.com/amalbinessa/re_streamlit/blob/main/Data/T_dataframe.csv"
-
-
-# read csv from a URL
-
-
-
-import csv
-
-with open(dataset_url, mode='w') as employee_file:
-    employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
 
 
