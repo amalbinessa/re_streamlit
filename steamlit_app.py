0aa8e1d681a7c8a57ab6c96087ef91c50a13b223
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 22:59:49 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -16,4 +16,20 @@ dataset_url ="https://github.com/amalbinessa/re_streamlit/blob/main/Data/T_dataf
 # read csv from a URL
 
 df = pd.read_csv(dataset_url,lineterminator='\n', sep=';')
+
+
+import csv
+
+with open('dataset_url') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
 st.write(df) 
