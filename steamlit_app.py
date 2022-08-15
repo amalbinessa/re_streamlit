b08d4c0b34b77e9ffb20339c0a9845399e783c34
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 23:20:25 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -10,6 +10,35 @@ st.set_page_config(
 
 st.title("ابحث عنك في قوقل")
 
+import requests
+from bs4 import BeautifulSoup
+import re
+
+query = "شركة ثقة لخدمات الأعمال"
+search = query.replace(' ', '+')
+results = 10
+url = (f"https://www.google.com/search?q={search}&num={results}")
+
+requests_results = requests.get(url)
+soup_link = BeautifulSoup(requests_results.content, "html.parser")
+links = soup_link.find_all("a")
+#links_list = []
+title_link_list = []
+
+for link in links:
+    link_href = link.get('href')
+    if "url?q=" in link_href and not "webcache" in link_href:
+      title = link.find_all('h3')
+
+      if len(title) > 0:
+          title_list = []
+          print(title[0].getText())
+          title_list.append(title[0].getText())
+          print(link.get('href').split("?q=")[1].split("&sa=U")[0])
+          title_list.append(link.get('href').split("?q=")[1].split("&sa=U")[0])
+          
+          print("------")
+          title_link_list.append(title_list)
 dataset_url ="https://github.com/amalbinessa/re_streamlit/blob/main/Data/T_dataframe.csv"
 
 
