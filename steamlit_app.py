6c6ee19a937f3895a3cb8c1ef7ece39257556b4b
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 22:37:34 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -16,7 +16,7 @@ dataset_url ="https://github.com/amalbinessa/re_streamlit/blob/main/T_dataframe.
 # read csv from a URL
 @st.experimental_memo
 def get_data() -> pd.DataFrame:
-    return pd.read_excel(dataset_url)
+    return pd.read_excel(dataset_url, encoding='utf8')
 
 df = get_data()
 
