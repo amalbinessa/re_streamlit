d14a53179ccee2aa4a79ac7ecc687266e2ce5136
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 21:51:44 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,16 +3,16 @@ import pandas as pd
 import numpy as np
 # add app title
 st.set_page_config(
-    page_title="نتائج مخزنة",
+    page_title="ماذا يقول عنك محرك البحث قوقل",
     page_icon="✅",
     layout="wide",
 )
 
 
-
-dataset_url = "T_dataframe.xlsx"
+dataset_url = "https://raw.githubusercontent.com/Lexie88rus/bank-marketing-analysis/master/bank.csv"
 
 # read csv from a URL
+@st.experimental_memo
 def get_data() -> pd.DataFrame:
     return pd.read_csv(dataset_url)
 
