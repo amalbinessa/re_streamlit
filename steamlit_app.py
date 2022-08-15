90b1255e78aea23a1d6d863b285a0a46631a2c3b
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-15 22:41:00 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -10,13 +10,13 @@ st.set_page_config(
 
 st.title("ابحث عنك في قوقل")
 
-dataset_url ="https://github.com/amalbinessa/re_streamlit/blob/main/T_dataframe.xlsx"
+dataset_url ="https://github.com/amalbinessa/re_streamlit/blob/main/T_dataframe.csv"
 
 
 # read csv from a URL
 @st.experimental_memo
 def get_data() -> pd.DataFrame:
-    return pd.read_excel(dataset_url, encoding='utf8')
+    return pd.read_csv(dataset_url, encoding='utf8')
 
 df = get_data()
 
