804ad2f6472f83abb8a6351ff519f1af9cb4e113
Amal Bin-Essa <55090554+amalbinessa@users.noreply.github.com>
2022-08-17 08:55:40 +0300

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -161,7 +161,7 @@ from arabic_reshaper import arabic_reshaper
 from bidi.algorithm import get_display
 import os
 from nltk.corpus import stopwords
-impot nltk
+import nltk
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 S# remove stop words:
